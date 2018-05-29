b07e2d59cd113301759e100f95ebd16c245a2b5b
Flavio Oliveira <flavio.m@samsung.com>
2018-05-29 16:25:03 -0300

    client/server socket connection sample

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
-const express = require('express');
 
+const http = require('http');
+const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
+const socketIO = require('socket.io')
 
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
 
 
 app.get('/', (req, res) => {
@@ -15,7 +27,7 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Listening on ${port}`);
 });
 
